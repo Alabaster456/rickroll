e7f9afbab9794adff8979745c73124154e4dcb02
Yazid <Alabaster456@users.noreply.github.com>
2014-06-29 09:08:02 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
-var audio = new Audio("http://www.here-and-now.info/audio/rickastley_artists.mp3");
+//Audio that plays over and over again (Rick Astley's song).
+ audio = new Audio("http://www.here-and-now.info/audio/rickastley_artists.mp3");
 audio.loop = true;
 audio.play();
